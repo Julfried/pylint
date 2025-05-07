573b28c905d253151f3fe436719c1bab43c6d746
Julfried <julian.grimm8@gmail.com>
2025-05-07 23:40:42 +0200

    Add test cases for filtering aggregations

---
+++ tests/pyreverse/functional/class_diagrams/aggregation_filtering/special.py
@@ -0,0 +1,22 @@
+# Test for https://github.com/pylint-dev/pylint/issues/10373
+# Aggregation relationships should be filtered according to attribute visibility
+
+class P:
+    def __init__(self, name: str = ""):
+        self.name = name
+
+class PublicAttr:
+    def __init__(self):
+        self.x: P = P("public")
+
+class ProtectedAttr:
+    def __init__(self):
+        self._x: P = P("protected")
+
+class PrivateAttr:
+    def __init__(self):
+        self.__x: P = P("private")
+
+class SpecialAttr:
+    def __init__(self):
+        self.__x__: P = P("special")
